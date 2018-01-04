c56e6a16f280af4ca9d36caffaaa2a62ddbd1293
Scott C. Livingston <scott@rerobots.net>
2018-01-04 14:11:26 -0800

    bump version number for release

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('README.rst') as fp:
 
 
 setup(name='rerobots',
-      version='0.0.0.dev0',
+      version='0.0.0',
       author='Scott C. Livingston',
       description='client library for the rerobots API',
       long_description=long_description,
