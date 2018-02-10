d1bcad7b971cca755be298e123387d614d06d092
Scott C. Livingston <scott@rerobots.net>
2018-02-10 15:38:10 -0800

    TEST: anonymous, arbitrary get_deployment_info()

--- tests/test_anonymous.py
+++ tests/test_anonymous.py
@@ -18,6 +18,20 @@ def test_deployments_list():
     # imply that rerobots has no active workspace deployments, which
     # should be rare or never.
 
+def test_deployment_details():
+    apic = APIClient()
+    workspace_deployments, page_count = apic.get_deployments(page=1, max_per_page=1)
+
+    # This assertion is redundant with test_deployments_list(), but it
+    # is among preconditions for the test below.
+    assert len(workspace_deployments) > 0
+
+    wdeployment_id = workspace_deployments[0]
+    details = apic.get_deployment_info(wdeployment_id)
+    assert 'id' in details
+    assert 'type' in details
+
+
 def test_instances_list():
     apic = APIClient()
     with assert_raises(WrongAuthToken):
