f18d5f523a289adc27ba38f188a8543e61fc45ed
Scott C. Livingston <scott@rerobots.net>
2018-04-11 14:42:07 -0700

    REL: version 0.3

--- setup.py
+++ setup.py
@@ -20,9 +20,9 @@ with open('README.rst') as fp:
 
 # Version of this package
 MAJOR=0
-MINOR=2
-PATCH=1
-devel=True
+MINOR=3
+PATCH=0
+devel=False
 
 version = '{}.{}.{}'.format(MAJOR, MINOR, PATCH)
 if devel:
