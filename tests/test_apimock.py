4b199f33399ad2249ed7472ad979a8dbef33a1cc
Scott C. Livingston <scott@rerobots.net>
2018-10-01 19:41:14 -0400

    TEST: mock case of request for instance failing
    because workspace deployment is busy, but user does not want a
    reservation to be created.

--- tests/test_apimock.py
+++ tests/test_apimock.py
@@ -4,9 +4,11 @@
 SCL <scott@rerobots.net>
 Copyright (c) 2018 rerobots, Inc.
 """
+from nose.tools import assert_raises
 import responses
 
 from rerobots.api import APIClient
+from rerobots.api import Error
 
 
 @responses.activate
@@ -23,8 +25,16 @@ def test_deployments_list():
 @responses.activate
 def test_request_instance():
     responses.add(responses.POST, 'https://api.rerobots.net/new/{}'.format('a6b88b4f-2402-41e4-8e81-b2fd852435eb'),
-                  json={'id': 'c81613e1-2d4c-4751-b3bc-08e604656c2d'},
+                  json={'id': 'c81613e1-2d4c-4751-b3bc-08e604656c2d',
+                        'success': True},
                   status=200)
+    responses.add(responses.POST, 'https://api.rerobots.net/new/{}'.format('a6b88b4f-2402-41e4-8e81-b2fd852435eb'),
+                  json={'result_message': ('All matching workspace deployments are busy.'
+                                           ' Try again later.')},
+                  status=503)
     apic = APIClient()
-    res = apic.request_instance('a6b88b4f-2402-41e4-8e81-b2fd852435eb')
+    res = apic.request_instance('a6b88b4f-2402-41e4-8e81-b2fd852435eb', reserve=False)
+    assert res['success']
     assert 'id' in res
+    with assert_raises(Error):
+        apic.request_instance('a6b88b4f-2402-41e4-8e81-b2fd852435eb', reserve=False)
