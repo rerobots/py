8d652507c3fcb5a32efcd1657efb8b45b5de76ba
Scott C. Livingston <scott@rerobots.net>
2018-10-02 23:58:53 -0400

    TEST: refactor test_request_instance into fixture
    [ci skip]

--- tests/test_apimock.py
+++ tests/test_apimock.py
@@ -4,6 +4,8 @@
 SCL <scott@rerobots.net>
 Copyright (c) 2018 rerobots, Inc.
 """
+import unittest
+
 from nose.tools import assert_raises
 import responses
 
@@ -22,19 +24,26 @@ def test_deployments_list():
     assert len(res) > 0
 
 
-@responses.activate
-def test_request_instance():
-    responses.add(responses.POST, 'https://api.rerobots.net/new/{}'.format('a6b88b4f-2402-41e4-8e81-b2fd852435eb'),
-                  json={'id': 'c81613e1-2d4c-4751-b3bc-08e604656c2d',
-                        'success': True},
-                  status=200)
-    responses.add(responses.POST, 'https://api.rerobots.net/new/{}'.format('a6b88b4f-2402-41e4-8e81-b2fd852435eb'),
-                  json={'result_message': ('All matching workspace deployments are busy.'
-                                           ' Try again later.')},
-                  status=503)
-    apic = APIClient()
-    res = apic.request_instance('a6b88b4f-2402-41e4-8e81-b2fd852435eb', reserve=False)
-    assert res['success']
-    assert 'id' in res
-    with assert_raises(Error):
-        apic.request_instance('a6b88b4f-2402-41e4-8e81-b2fd852435eb', reserve=False)
+class BasicInstanceTestCases(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @responses.activate
+    def test_request_instance(self):
+        responses.add(responses.POST, 'https://api.rerobots.net/new/{}'.format('a6b88b4f-2402-41e4-8e81-b2fd852435eb'),
+                      json={'id': 'c81613e1-2d4c-4751-b3bc-08e604656c2d',
+                            'success': True},
+                      status=200)
+        responses.add(responses.POST, 'https://api.rerobots.net/new/{}'.format('a6b88b4f-2402-41e4-8e81-b2fd852435eb'),
+                      json={'result_message': ('All matching workspace deployments are busy.'
+                                               ' Try again later.')},
+                      status=503)
+        apic = APIClient()
+        res = apic.request_instance('a6b88b4f-2402-41e4-8e81-b2fd852435eb', reserve=False)
+        assert res['success']
+        assert 'id' in res
+        with assert_raises(Error):
+            apic.request_instance('a6b88b4f-2402-41e4-8e81-b2fd852435eb', reserve=False)
