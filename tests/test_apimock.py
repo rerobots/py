dae8ddf5a88bbcc9d6bd25c351a3d8695ee2bc93
Scott C. Livingston <scott@rerobots.net>
2018-10-07 20:04:25 -0400

    TEST: create callback for new instance test fixture

--- tests/test_apimock.py
+++ tests/test_apimock.py
@@ -4,6 +4,7 @@
 SCL <scott@rerobots.net>
 Copyright (c) 2018 rerobots, Inc.
 """
+import json
 import unittest
 
 from nose.tools import assert_raises
@@ -26,23 +27,33 @@ def test_deployments_list():
 
 class BasicInstanceTestCases(unittest.TestCase):
     def setUp(self):
-        wdeployment_id = 'a6b88b4f-2402-41e4-8e81-b2fd852435eb'
-        instance_id = 'c81613e1-2d4c-4751-b3bc-08e604656c2d'
+        self.wdeployment_id = 'a6b88b4f-2402-41e4-8e81-b2fd852435eb'
+        self.instance_ids_stack = ['c81613e1-2d4c-4751-b3bc-08e604656c2d']
+        self.active_instances = []
         responses.add(responses.GET, 'https://api.rerobots.net/deployments',
-                      json={'workspace_deployments': [wdeployment_id],
+                      json={'workspace_deployments': [self.wdeployment_id],
                             'page_count': 1},
                       status=200)
-        responses.add(responses.POST, 'https://api.rerobots.net/new/{}'.format(wdeployment_id),
-                      json={'id': instance_id,
-                            'success': True},
-                      status=200)
-        responses.add(responses.POST, 'https://api.rerobots.net/new/{}'.format(wdeployment_id),
-                      json={'result_message': ('All matching workspace deployments are busy.'
-                                               ' Try again later.')},
-                      status=503)
+        responses.add_callback(responses.POST,
+                               'https://api.rerobots.net/new/{}'.format(self.wdeployment_id),
+                               callback=self._new_callback,
+                               content_type='application/json')
 
     def tearDown(self):
         pass
+
+    def _new_callback(self, request):
+        headers = dict()
+        if len(self.instance_ids_stack) == 0:
+            message = 'All matching workspace deployments are busy. Try again later.'
+            return (503, headers, json.dumps({'result_message': message}))
+        else:
+            self.active_instances.append(self.instance_ids_stack.pop())
+            payload = {
+                'id': self.active_instances[-1],
+                'success': True,
+            }
+            return (200, headers, json.dumps(payload))
 
     @responses.activate
     def test_request_instance(self):
