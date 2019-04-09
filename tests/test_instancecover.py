183f2d535befb998ecec3c147139884af513a5b3
Scott C. Livingston <scott@rerobots.net>
2019-04-09 08:56:05 -0700

    TEST: add file write-read to test_instance_start_terminate
    not included in suite run on Travis CI, so
    [ci skip]

--- tests/test_instancecover.py
+++ tests/test_instancecover.py
@@ -12,36 +12,73 @@ SCL <scott@rerobots.net>
 Copyright (c) 2019 rerobots, Inc.
 """
 import os
+import random
+import tempfile
 import time
 
 import pytest
 
-from rerobots.api import APIClient
-from rerobots.api import BusyWorkspaceDeployment, BusyWorkspaceInstance, WrongAuthToken
+from rerobots.api import APIClient, Instance
+from rerobots.api import BusyWorkspaceDeployment, BusyWorkspaceInstance
 
 
 def test_instance_start_terminate():
+    # Instantiate from among a given feasible list
     candidate_wtypes = [
         'fixed_misty2fieldtrial',
     ]
-    apic = APIClient()
-    available_wdeployments = apic.get_deployments(types=candidate_wtypes, maxlen=0)
+    available_wdeployments = APIClient().get_deployments(types=candidate_wtypes, maxlen=0)
     assert len(available_wdeployments) > 0
-    payload = None
+    inst = None
     for instantiate_attempt in range(5):
         for wdeployment_id in available_wdeployments:
             try:
-                payload = apic.request_instance(wdeployment_id, reserve=False)
+                inst = Instance(wdeployment_id=wdeployment_id)
             except BusyWorkspaceDeployment:
                 continue
             break
-        if payload and payload['success']:
+        if inst is not None:
             break
-    assert payload['success']
+    assert inst is not None
+
+    # Wait for instance to be READY
+    start_time = time.monotonic()
+    while time.monotonic() - start_time < 180:
+        if inst.get_status() == 'READY':
+            ready_become_duration = time.monotonic() - start_time
+            print('become ready duration: {}'.format(ready_become_duration))
+            break
+        time.sleep(2)
+    if inst.get_status() != 'READY':
+        raise Exception('timed out waiting for instance to become READY')
+
+    # Put file, and ready it back
+    rdata = bytes([random.randint(0, 255) for x in range(16)])
+    fd, fname = tempfile.mkstemp()
+    fp = os.fdopen(fd, 'wb')
+    fp.write(rdata)
+    fp.close()
+    remotepath = '/root/{}'.format(os.path.basename(fname))
+    localdir = tempfile.mkdtemp()
+    newlocalpath = os.path.join(localdir, 'out')
+    start_time = time.monotonic()
+    inst.start_sshclient()
+    inst.put_file(fname, remotepath)
+    inst.get_file(remotepath, newlocalpath)
+    file_cycle_duration = time.monotonic() - start_time
+    print('cycle file duration: {}'.format(file_cycle_duration))
+    with open(newlocalpath, 'rb') as fp:
+        rdata_back = fp.read()
+    assert rdata == rdata_back
+    os.unlink(fname)
+    os.unlink(newlocalpath)
+    os.rmdir(localdir)
+
+    # Terminate
     terminated = False
     for terminate_attempt in range(7):
         try:
-            apic.terminate_instance(payload['id'])
+            inst.terminate()
             terminated = True
             break
         except BusyWorkspaceInstance as e:
